bb64e31061232d5378cf0f795b6907574732a7b6
System Administrator <root@192-168-1-115.tpgi.com.au>
2020-01-29 00:08:50 +1000

    Updates

---
+++ precache-manifest.41a65141b86ae3af86a1f6a81eab9ed1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3867cffe832394a68e0eef5b538bb24b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "706543f92de951033ab2",
+    "url": "/my-app/static/css/main.67d839ee.chunk.css"
+  },
+  {
+    "revision": "2a3c4cbc2dbe29cd2dbf",
+    "url": "/my-app/static/js/2.c83deedc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.c83deedc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "706543f92de951033ab2",
+    "url": "/my-app/static/js/main.29b58e44.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
